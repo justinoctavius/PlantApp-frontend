6b479c1141ec5ff4814d81dfeaa3c8e29b4f2658
octavius2929 <justinoctavio2001@gmail.com>
2021-04-06 16:14:00 -0400

    changin the icon splash

--- app/config/envs/dev.js
+++ app/config/envs/dev.js
@@ -1,6 +1,6 @@
 const development = {
-  BACKEND_API: 'http://10.0.0.212:8080/api',
-  BACKEND_IMAGE: 'http://10.0.0.212:8080',
+  BACKEND_API: 'http://10.0.0.212/api',
+  BACKEND_IMAGE: 'http://10.0.0.212',
 };
 
 export default development;
